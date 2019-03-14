4cef0711390318d237f16670c42f7e185741a5ff
Pham Dat <phamdat@gmail.com>
2019-03-14 11:52:57 +0700

    first commit

---
+++ src/main/java/com/example/bootemo/BootemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.bootemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootemoApplication.class, args);
+	}
+
+}
+
