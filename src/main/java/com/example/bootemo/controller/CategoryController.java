3efc48d540d294e389e685930a2efbc85f9e4f07
dat <phamdatvmu95@gmail.com>
2019-03-21 09:19:06 +0700

    sprin 1

---
+++ src/main/java/com/example/bootemo/controller/CategoryController.java
@@ -0,0 +1,38 @@
+package com.example.bootemo.controller;
+
+import com.example.bootemo.model.Blog;
+import com.example.bootemo.model.Category;
+import com.example.bootemo.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @RequestMapping(value = "/category/", method = RequestMethod.POST)
+    public ResponseEntity<Void> createCategory(@RequestBody Category category, UriComponentsBuilder ucBuilder) {
+        System.out.println("Creating blog " + category.getCategory());
+        categoryService.create(category);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/category/{id}").buildAndExpand(category.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/category/",method = RequestMethod.GET)
+    public ResponseEntity<List<Category>> listAllCategory(){
+        List<Category> categories = categoryService.findAll();
+        if (categories.isEmpty()){
+            return new ResponseEntity<List<Category>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Category>>(categories, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/bootemo/model/Category.java
@@ -0,0 +1,49 @@
+package com.example.bootemo.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void setBlogs(List<Blog> blogs) {
+        this.blogs = blogs;
+    }
+
+    public Category() {
+    }
+
+    public Category(String category, List<Blog> blogs) {
+        this.category = category;
+        this.blogs = blogs;
+    }
+
+    private String category;
+
+    @OneToMany(targetEntity = Blog.class)
+    private List<Blog> blogs;
+}
